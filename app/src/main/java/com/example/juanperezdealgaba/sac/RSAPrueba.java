74e2ba64d0ffe18e28d8c2999c153f13e010f418
Juan I. Pérez de Algaba Sierra <juan.perezalgaba@gmail.com>
2018-06-06 14:21:48 +0200

    RSA BC

---
+++ app/src/main/java/com/example/juanperezdealgaba/sac/RSAPrueba.java
@@ -0,0 +1,268 @@
+package com.example.juanperezdealgaba.sac;
+
+import android.util.Base64;
+import android.widget.TextView;
+
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.security.Key;
+import java.security.KeyFactory;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+
+import java.security.spec.EncodedKeySpec;
+
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+
+public class RSAPrueba {
+
+    public void testRSA(FileWriter writer, TextView results, int blocksize){
+        Security.addProvider(new BouncyCastleProvider());
+
+        try {
+            String private_key = "MIIEowIBAAKCAQEAy8Dbv8prpJ/0kKhlGeJYozo2t60EG8L0561g13R29LvMR5hy\n" +
+                    "vGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+vw1HocOAZtWK0z3r26uA8kQYOKX9\n" +
+                    "Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQApfc9jB9nTzphOgM4JiEYvlV8FLhg9\n" +
+                    "yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68i6T4nNq7NWC+UNVjQHxNQMQMzU6l\n" +
+                    "WCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoVPpY72+eVthKzpMeyHkBn7ciumk5q\n" +
+                    "gLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUywQIDAQABAoIBADhg1u1Mv1hAAlX8\n" +
+                    "omz1Gn2f4AAW2aos2cM5UDCNw1SYmj+9SRIkaxjRsE/C4o9sw1oxrg1/z6kajV0e\n" +
+                    "N/t008FdlVKHXAIYWF93JMoVvIpMmT8jft6AN/y3NMpivgt2inmmEJZYNioFJKZG\n" +
+                    "X+/vKYvsVISZm2fw8NfnKvAQK55yu+GRWBZGOeS9K+LbYvOwcrjKhHz66m4bedKd\n" +
+                    "gVAix6NE5iwmjNXktSQlJMCjbtdNXg/xo1/G4kG2p/MO1HLcKfe1N5FgBiXj3Qjl\n" +
+                    "vgvjJZkh1as2KTgaPOBqZaP03738VnYg23ISyvfT/teArVGtxrmFP7939EvJFKpF\n" +
+                    "1wTxuDkCgYEA7t0DR37zt+dEJy+5vm7zSmN97VenwQJFWMiulkHGa0yU3lLasxxu\n" +
+                    "m0oUtndIjenIvSx6t3Y+agK2F3EPbb0AZ5wZ1p1IXs4vktgeQwSSBdqcM8LZFDvZ\n" +
+                    "uPboQnJoRdIkd62XnP5ekIEIBAfOp8v2wFpSfE7nNH2u4CpAXNSF9HsCgYEA2l8D\n" +
+                    "JrDE5m9Kkn+J4l+AdGfeBL1igPF3DnuPoV67BpgiaAgI4h25UJzXiDKKoa706S0D\n" +
+                    "4XB74zOLX11MaGPMIdhlG+SgeQfNoC5lE4ZWXNyESJH1SVgRGT9nBC2vtL6bxCVV\n" +
+                    "WBkTeC5D6c/QXcai6yw6OYyNNdp0uznKURe1xvMCgYBVYYcEjWqMuAvyferFGV+5\n" +
+                    "nWqr5gM+yJMFM2bEqupD/HHSLoeiMm2O8KIKvwSeRYzNohKTdZ7FwgZYxr8fGMoG\n" +
+                    "PxQ1VK9DxCvZL4tRpVaU5Rmknud9hg9DQG6xIbgIDR+f79sb8QjYWmcFGc1SyWOA\n" +
+                    "SkjlykZ2yt4xnqi3BfiD9QKBgGqLgRYXmXp1QoVIBRaWUi55nzHg1XbkWZqPXvz1\n" +
+                    "I3uMLv1jLjJlHk3euKqTPmC05HoApKwSHeA0/gOBmg404xyAYJTDcCidTg6hlF96\n" +
+                    "ZBja3xApZuxqM62F6dV4FQqzFX0WWhWp5n301N33r0qR6FumMKJzmVJ1TA8tmzEF\n" +
+                    "yINRAoGBAJqioYs8rK6eXzA8ywYLjqTLu/yQSLBn/4ta36K8DyCoLNlNxSuox+A5\n" +
+                    "w6z2vEfRVQDq4Hm4vBzjdi3QfYLNkTiTqLcvgWZ+eX44ogXtdTDO7c+GeMKWz4XX\n" +
+                    "uJSUVL5+CVjKLjZEJ6Qc2WZLl94xSwL71E41H4YciVnSCQxVc4Jw";
+
+            String public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy8Dbv8prpJ/0kKhlGeJY\n" +
+                    "ozo2t60EG8L0561g13R29LvMR5hyvGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+\n" +
+                    "vw1HocOAZtWK0z3r26uA8kQYOKX9Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQAp\n" +
+                    "fc9jB9nTzphOgM4JiEYvlV8FLhg9yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68\n" +
+                    "i6T4nNq7NWC+UNVjQHxNQMQMzU6lWCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoV\n" +
+                    "PpY72+eVthKzpMeyHkBn7ciumk5qgLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUy\n" +
+                    "wQIDAQAB";
+
+            PrivateKey private_key_ready = getPrivateKeyFromString(private_key);
+            PublicKey public_key_ready = getPublicKeyFromString(public_key);
+
+            RandomStringGenerator string = new RandomStringGenerator();
+
+            byte[] input = string.generateRandomString(blocksize).getBytes();
+
+            byte[] encrypted = encrypt(input,public_key_ready);
+            byte[] decrypted = decrypt(encrypted,private_key_ready);
+            String decrypted_fin = new String(decrypted,"UTF-8");
+            System.out.println("We start here:");
+            System.out.println(decrypted_fin);
+
+        }catch (Exception o){
+            throw new RuntimeException(o);
+        }
+    }
+
+    public void testRSA(){
+        Security.addProvider(new BouncyCastleProvider());
+
+        try {
+            String private_key = "MIIEowIBAAKCAQEAy8Dbv8prpJ/0kKhlGeJYozo2t60EG8L0561g13R29LvMR5hy\n" +
+                    "vGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+vw1HocOAZtWK0z3r26uA8kQYOKX9\n" +
+                    "Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQApfc9jB9nTzphOgM4JiEYvlV8FLhg9\n" +
+                    "yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68i6T4nNq7NWC+UNVjQHxNQMQMzU6l\n" +
+                    "WCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoVPpY72+eVthKzpMeyHkBn7ciumk5q\n" +
+                    "gLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUywQIDAQABAoIBADhg1u1Mv1hAAlX8\n" +
+                    "omz1Gn2f4AAW2aos2cM5UDCNw1SYmj+9SRIkaxjRsE/C4o9sw1oxrg1/z6kajV0e\n" +
+                    "N/t008FdlVKHXAIYWF93JMoVvIpMmT8jft6AN/y3NMpivgt2inmmEJZYNioFJKZG\n" +
+                    "X+/vKYvsVISZm2fw8NfnKvAQK55yu+GRWBZGOeS9K+LbYvOwcrjKhHz66m4bedKd\n" +
+                    "gVAix6NE5iwmjNXktSQlJMCjbtdNXg/xo1/G4kG2p/MO1HLcKfe1N5FgBiXj3Qjl\n" +
+                    "vgvjJZkh1as2KTgaPOBqZaP03738VnYg23ISyvfT/teArVGtxrmFP7939EvJFKpF\n" +
+                    "1wTxuDkCgYEA7t0DR37zt+dEJy+5vm7zSmN97VenwQJFWMiulkHGa0yU3lLasxxu\n" +
+                    "m0oUtndIjenIvSx6t3Y+agK2F3EPbb0AZ5wZ1p1IXs4vktgeQwSSBdqcM8LZFDvZ\n" +
+                    "uPboQnJoRdIkd62XnP5ekIEIBAfOp8v2wFpSfE7nNH2u4CpAXNSF9HsCgYEA2l8D\n" +
+                    "JrDE5m9Kkn+J4l+AdGfeBL1igPF3DnuPoV67BpgiaAgI4h25UJzXiDKKoa706S0D\n" +
+                    "4XB74zOLX11MaGPMIdhlG+SgeQfNoC5lE4ZWXNyESJH1SVgRGT9nBC2vtL6bxCVV\n" +
+                    "WBkTeC5D6c/QXcai6yw6OYyNNdp0uznKURe1xvMCgYBVYYcEjWqMuAvyferFGV+5\n" +
+                    "nWqr5gM+yJMFM2bEqupD/HHSLoeiMm2O8KIKvwSeRYzNohKTdZ7FwgZYxr8fGMoG\n" +
+                    "PxQ1VK9DxCvZL4tRpVaU5Rmknud9hg9DQG6xIbgIDR+f79sb8QjYWmcFGc1SyWOA\n" +
+                    "SkjlykZ2yt4xnqi3BfiD9QKBgGqLgRYXmXp1QoVIBRaWUi55nzHg1XbkWZqPXvz1\n" +
+                    "I3uMLv1jLjJlHk3euKqTPmC05HoApKwSHeA0/gOBmg404xyAYJTDcCidTg6hlF96\n" +
+                    "ZBja3xApZuxqM62F6dV4FQqzFX0WWhWp5n301N33r0qR6FumMKJzmVJ1TA8tmzEF\n" +
+                    "yINRAoGBAJqioYs8rK6eXzA8ywYLjqTLu/yQSLBn/4ta36K8DyCoLNlNxSuox+A5\n" +
+                    "w6z2vEfRVQDq4Hm4vBzjdi3QfYLNkTiTqLcvgWZ+eX44ogXtdTDO7c+GeMKWz4XX\n" +
+                    "uJSUVL5+CVjKLjZEJ6Qc2WZLl94xSwL71E41H4YciVnSCQxVc4Jw";
+
+            String public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy8Dbv8prpJ/0kKhlGeJY\n" +
+                    "ozo2t60EG8L0561g13R29LvMR5hyvGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+\n" +
+                    "vw1HocOAZtWK0z3r26uA8kQYOKX9Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQAp\n" +
+                    "fc9jB9nTzphOgM4JiEYvlV8FLhg9yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68\n" +
+                    "i6T4nNq7NWC+UNVjQHxNQMQMzU6lWCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoV\n" +
+                    "PpY72+eVthKzpMeyHkBn7ciumk5qgLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUy\n" +
+                    "wQIDAQAB";
+
+            PrivateKey private_key_ready = getPrivateKeyFromString(private_key);
+            PublicKey public_key_ready = getPublicKeyFromString(public_key);
+
+            String prueba = "this is a test";
+
+            byte[] input = prueba.getBytes();
+
+            byte[] encrypted = encrypt(input,public_key_ready);
+            byte[] decrypted = decrypt(encrypted,private_key_ready);
+            String decrypted_fin = new String(decrypted,"UTF-8");
+            System.out.println("We start here:");
+            System.out.println(decrypted_fin);
+
+        }catch (Exception o){
+            throw new RuntimeException(o);
+        }
+    }
+
+    public static byte[] encrypt(byte[] text, PublicKey key) throws Exception
+    {
+        byte[] cipherText = null;
+        //
+        // get an RSA cipher object and print the provider
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        // encrypt the plaintext using the public key
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipherText = cipher.doFinal(text);
+        return cipherText;
+    }
+
+    /**
+     * Encrypt a text using public key. The result is enctypted BASE64 encoded text
+     * @param text The original unencrypted text
+     * @param key The public key
+     * @return Encrypted text encoded as BASE64
+     * @throws java.lang.Exception
+     */
+    public static String encrypt(String text, PublicKey key) throws Exception
+    {
+        String encryptedText;
+        byte[] cipherText = encrypt(text.getBytes("UTF8"),key);
+        encryptedText = encodeBASE64(cipherText);
+        return encryptedText;
+    }
+
+    /**
+     * Decrypt text using private key
+     * @param text The encrypted text
+     * @param key The private key
+     * @return The unencrypted text
+     * @throws java.lang.Exception
+     */
+    public static byte[] decrypt(byte[] text, PrivateKey key) throws Exception
+    {
+        byte[] dectyptedText = null;
+        // decrypt the text using the private key
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        dectyptedText = cipher.doFinal(text);
+        return dectyptedText;
+
+    }
+
+    /**
+     * Decrypt BASE64 encoded text using private key
+     * @param text The encrypted text, encoded as BASE64
+     * @param key The private key
+     * @return The unencrypted text encoded as UTF8
+     * @throws java.lang.Exception
+     */
+    public static String decrypt(String text, PrivateKey key) throws Exception
+    {
+        String result;
+        // decrypt the text using the private key
+        byte[] dectyptedText = decrypt(decodeBASE64(text),key);
+        result = new String(dectyptedText, "UTF8");
+        return result;
+
+    }
+
+    /**
+     * Convert a Key to string encoded as BASE64
+     * @param key The key (private or public)
+     * @return A string representation of the key
+     */
+    public static String getKeyAsString(Key key)
+    {
+        // Get the bytes of the key
+        byte[] keyBytes = key.getEncoded();
+        return encodeBASE64(keyBytes);
+    }
+
+    /**
+     * Generates Private Key from BASE64 encoded string
+     * @param key BASE64 encoded string which represents the key
+     * @return The PrivateKey
+     * @throws java.lang.Exception
+     */
+    public static PrivateKey getPrivateKeyFromString(String key) throws Exception
+    {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(decodeBASE64(key));
+        PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+        return privateKey;
+    }
+
+    /**
+     * Generates Public Key from BASE64 encoded string
+     * @param key BASE64 encoded string which represents the key
+     * @return The PublicKey
+     * @throws java.lang.Exception
+     */
+    public static PublicKey getPublicKeyFromString(String key) throws Exception
+    {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(decodeBASE64(key));
+        PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+        return publicKey;
+    }
+
+    /**
+     * Encode bytes array to BASE64 string
+     * @param bytes
+     * @return Encoded string
+     */
+    private static String encodeBASE64(byte[] bytes)
+    {
+        // BASE64Encoder b64 = new BASE64Encoder();
+        // return b64.encode(bytes, false);
+        return Base64.encodeToString(bytes,Base64.DEFAULT);
+    }
+
+    /**
+     * Decode BASE64 encoded string to bytes array
+     * @param text The string
+     * @return Bytes array
+     * @throws IOException
+     */
+    private static byte[] decodeBASE64(String text) throws IOException
+    {
+        // BASE64Decoder b64 = new BASE64Decoder();
+        // return b64.decodeBuffer(text);
+        return Base64.decode(text,Base64.DEFAULT);
+    }
+}
